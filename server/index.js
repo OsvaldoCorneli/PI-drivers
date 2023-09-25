9cba11372d89245fdedc91459e7c9f2429a8dbf4
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-09-25 16:38:06 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const axios = require("axios");
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const PORT = 3001;
+const {DB_NAME} = process.env;
+
+conn.sync({ force: true }).then(() => {
+  console.log(`Connected to the ${DB_NAME} database`);
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})
+}).catch(error => console.error(error))
